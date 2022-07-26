2f3199a169fd5d118811ec99ec3fc897db6ac857
Bobzero23 <rajabu.mustafa.00@gmail.com>
2022-07-26 20:13:12 +0300

    COMMIT 2013-26

---
+++ src/var_args/Var_args_multiplication.java
@@ -0,0 +1,32 @@
+package var_args;
+
+/*MULITIPLYING NUMBERS USING VAR ARGS*/
+
+/*main class*/
+public class Var_args_multiplication {
+
+	/*declaring a variable*/
+	int mult = 1; 
+	
+	/*var args method for multiplication*/
+	public int multiply(int...x) {
+		for(int i : x) {		
+			 mult = mult * i;
+			
+		}
+		return mult;
+	}
+	
+	/*main method*/
+	public static void main(String[] args) {
+		
+		/*creating a class*/
+		Var_args_multiplication object = new Var_args_multiplication();
+		
+		/*displaying the results*/
+		System.out.println(object.multiply(5, 5, 5));
+		
+		
+		}
+	
+}
