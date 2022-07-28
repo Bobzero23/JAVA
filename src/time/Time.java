f8a7556e7b62eef71e90e445c4efa647d304852e
Bobzero23 <rajabu.mustafa.00@gmail.com>
2022-07-28 09:55:59 +0300

    COMMIT 0953-28

---
+++ src/time/Time.java
@@ -0,0 +1,21 @@
+package time;
+
+/*HERE WE GONNA SEE HOW TO GET THE DATE AND TIME AND ALL OF THAT KINDA STUFFS*/
+
+/*must imports*/
+import java.util.*;
+
+/*main class*/
+public class Time {
+
+	/*main mehtod*/
+	public static void main(String[] args) {
+
+		/*this object will display the full date and time */
+		Date fullDate = new Date();
+		
+		/*displaying resutls*/
+		System.out.println(fullDate);
+	}
+
+}
