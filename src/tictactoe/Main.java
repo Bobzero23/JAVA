25b4c922df0c436df09271c139d4ada7cae05be3
Bobzero23 <rajabu.mustafa.00@gmail.com>
2022-08-02 19:26:54 +0300

    COMMIT 1926-2

---
+++ src/tictactoe/Main.java
@@ -0,0 +1,10 @@
+package tictactoe;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		 Tictactoe tictactoe = new Tictactoe();
+	}
+
+}
---
+++ src/tictactoe/Tictactoe.java
@@ -0,0 +1,74 @@
+package tictactoe;
+
+/*must imports*/
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/*tictactoe class*/
+public class Tictactoe implements ActionListener{
+
+	/*declaring needed variables*/
+	Random random = new Random();
+	JFrame frame = new JFrame();
+	JPanel title_panel = new JPanel(); //PANEL TO HOLD THE TITLE
+	JPanel button_panel = new JPanel(); //PANEL TO HOLD THE BUTTONS
+	JLabel textfield = new JLabel();
+	JButton[] button = new JButton[9];
+	boolean player1_turn;
+	
+	/*constructor*/
+	public Tictactoe() {
+
+		/*designing the frame*/
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600,600);
+		frame.getContentPane().setBackground(new Color(50, 50, 50));
+		frame.setLayout(new BorderLayout());
+		frame.setLocationRelativeTo(null);
+		
+		textfield.setBackground(new Color(25, 25, 25));
+		textfield.setForeground(new Color(25, 255, 0));
+		textfield.setFont(new Font("Ink Free", Font.BOLD, 75));
+		textfield.setHorizontalAlignment(JLabel.CENTER);
+		textfield.setText("TIC TAC TOE");
+		textfield.setOpaque(true);
+		
+		title_panel.setLayout(new BorderLayout());
+		title_panel.add(textfield);
+		frame.add(title_panel);
+		
+		
+		frame.setVisible(true);
+		
+	}
+
+	/*must implemented method frome actionlistener interface*/
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/*method to decide who starts first*/
+	public void firstTurn() {
+		
+	}
+	
+	/*method to check who wins*/
+	public void check() {
+		
+	}
+	
+	/*method to show if x wins*/
+	public void xWins(int a, int b, int c) {
+		
+	}
+	
+	/*method to show if o wins*/
+	public void oWins() {
+		
+	}
+	
+}
