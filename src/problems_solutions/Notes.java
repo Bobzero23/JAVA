69753795a38b21836f6795c17a68e0642a8a75b7
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-05 05:58:33 +0300

    COMMIT 0557-5

---
+++ src/problems_solutions/Notes.java
@@ -0,0 +1,50 @@
+/* WHY DELETING ECLIPSE ON DESKTOP ALSO DELECTES THE REAL ECLIPSE
+ * --> Because you saved it as you eclipse installation path next time change the installation
+ * path when you install eclipse
+ */
+
+/* WHERE CAN I GET MY ECLIPSE PROJECTS AFTER I DELETE ECLIPSE
+ * --> Github
+ */
+
+/* WHAT TO DO, I CANT FIND THE SERVER TAG IN ECLIPSE 
+ * Help -> Install New Software
+ * Choose "Luna - http://download.eclipse.org/releases/luna" site
+ * Expand "Web, XML, and Java EE Development"
+ * Check JST Server Adapters and JST Server Adapters Extentions
+ */
+
+/* WHY ECLIPSE WONT SHOW THE VERSION OF THE TOMCAT I INSTALLED
+ * help-->update new softwares
+ * then update those folders 
+ * restart eclipse and boom
+ */
+
+/* WHAT TO DO, MY TOMCAT WONT WORK
+ * --> read the error and if the error says fail to start
+ * delete the tomcat on ther server tab
+ * and also delete tomcat runtime and reset up tomcat again
+ *--> if the error says tomcat is being used 
+ * jsut change the tomcat port number by double clicking the tomcat in the server tab
+ * there you will see the port numbe and change it  
+ */
+
+/* MY PROJECT GIVE ERRORS AFTER I DELETED TOMCAT
+ * click the project and build a path
+ * there go add a new library 
+ * choose server and add tomcat
+ * that is all you gotta do
+ */
+
+/* WHY GITHUB WONT COUNT MY CONTRIBUTIONS
+ * because you didnt connect you github user name and email to eclipse 
+ * to do that search for git configuration 
+ * there go add add entry 
+ * for the key type user.name 
+ * and value must be your github username
+ * appl and save and add another entry there
+ * for the key must user.email
+ * and value must be your github user email
+ * apply and save
+ */
+ 
