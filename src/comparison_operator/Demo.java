855578fa7c2c082510b56c84288092c2b7e6e9db
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-05 21:24:15 +0300

    COMMIT 2124-5

---
+++ src/comparison_operator/Demo.java
@@ -0,0 +1,20 @@
+package comparison_operator;
+
+/*InstanceOf is well known as comparison operator, it is used to chek if the object is of the gibven class, interface*/
+/*it is a boolean operator so it either returns true or false*/
+
+/*main class*/
+public class Demo {
+
+	/*main method*/
+	public static void main(String[] args) {
+	
+		/*creating object of the class*/
+		Demo object = new Demo();
+
+		/*using the comparison operator*/
+		System.out.println(object instanceof Demo );
+	
+	}
+
+}
