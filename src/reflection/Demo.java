445ef1560ccc518e9b957a42b7dc1ff609de4504
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-06 21:13:02 +0300

    COMMIT 2113-6

---
+++ src/reflection/Demo.java
@@ -0,0 +1,21 @@
+package reflection;
+
+/*Reflection can be used to get information about class, constructors, and methods
+ * Through reflection, we can access the private variables and methods of a class 
+ * with the help of its class object and invoke the method*/
+
+/*test class*/
+class Test{
+	
+}
+/*main class*/
+public class Demo {
+
+	/*main deriver class*/
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
