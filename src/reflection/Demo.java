228017506df4ebbf4a64cfd0906a22cf020edad9
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-07 10:18:06 +0300

    COMMIT 1017-7

--- src/reflection/Demo.java
+++ src/reflection/Demo.java
@@ -1,13 +1,20 @@
 package reflection;
 
-/*Reflection can be used to get information about class, constructors, and methods
+/* Reflection can be used to get information about class, constructors, and methods
  * Through reflection, we can access the private variables and methods of a class 
  * with the help of its class object and invoke the method*/
 
-/*must imports*/
+/*must imports reflect*/
 
 /*test class*/
 class Test{
+	
+	/*creating a private field*/
+	/*creating a constructor*/
+	/*initializing a value of the field inside the constructor*/
+	/*creating a method that will print the value of the field*/
+	/*creating a method that takes a value as parameter and display the value inside the method*/
+	/*creating a private method which prints anything*/
 	
 }
 
@@ -18,6 +25,9 @@ public class Demo {
 	/*main deriver class*/
 	public static void main(String[] args) {
 
+		/*creating the object of the test class*/
+		/*creating object of the demo class to get the class*/
+		/*using the the object we created to get the name of the class*/
 		
 		
 	}
