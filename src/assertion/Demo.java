86fd92734cee198f3d93545857a6f546dd2b7861
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-08 21:00:23 +0300

    I AM DONE WITH THIS ASSERTION STUFF

--- src/assertion/Demo.java
+++ src/assertion/Demo.java
@@ -12,7 +12,7 @@ public class Demo {
 		int year = 2000;
 		
 		/*using assertion expression: expression;*/
-		assert year > 2005 : "Won't attend the race";
+		assert year <= 2005 : "Won't attend the race";
 		
 		/*displaying the result*/
 		System.out.println("The year is: " + year);
