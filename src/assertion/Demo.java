1b51a5fd7439cdb6cb25baeba5884813e68cdc65
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-08 20:54:53 +0300

    WORKING ON ASSERTION

---
+++ src/assertion/Demo.java
@@ -0,0 +1,22 @@
+package assertion;
+
+/*Assertion test the correctness of the program using assert statement in Java*/
+
+/*main class*/
+public class Demo {
+
+	/*main derived method*/
+	public static void main(String[] args) {
+
+		/*initializing a variable*/
+		int year = 2000;
+		
+		/*using assertion expression: expression;*/
+		assert year > 2005 : "Won't attend the race";
+		
+		/*displaying the result*/
+		System.out.println("The year is: " + year);
+	
+	}
+
+}
