4539683da786d2a9b48dfc5d6e07cbdec85d4ee9
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-08 20:22:30 +0300

    DONE WITH LOGGING NOW

--- src/logging/Demo.java
+++ src/logging/Demo.java
@@ -8,18 +8,41 @@ package logging;
 /*must imports*/
 import java.util.logging.*;
 
+class DemoLogger {
+	
+	/*creating a static object because log manager is universal*/
+	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	
+	public void makeSomeLog() {
+		LOGGER.log(Level.INFO,"My first Log");
+	}
+}
+
 /*mian class*/
 public class Demo {
 	
 	/*main driver method*/
 	public static void main(String[] args) {
 
-		/*creating a static object because log manager is universal*/
-		private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+		/*creating object of the DemoLogger class*/
+		DemoLogger demologger = new DemoLogger();
 		
-		public void makeSomeLog() {
-			System.out.println();
-		}
+		/*calling the method inside it using the object we created*/
+		demologger.makeSomeLog();
+		
+		/*creating object of the LogManager and getLogManager*/
+		LogManager logmanager = LogManager.getLogManager();
+		
+		/*creating object of the Logger and getLogger using the LogManager object*/
+		/*whenever we create object of the Logger class we should put this Logger.GLOBAL_LOGGER_NAME
+		 *  as parameter fo the constructor*/
+		Logger logger = logmanager.getLogger(Logger.GLOBAL_LOGGER_NAME);
+		
+		/*using the object of Logger class and log method we get the logger now*/
+		/*I think there must be level.INFO whenever we use object of Logge class*/
+		logger.log(Level.INFO, "This is my second Log");
+		
+		
 	}
 
 }
