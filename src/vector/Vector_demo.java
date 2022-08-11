746a604e60e01a2ceac59c7007253e17dc2c73a2
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-11 21:39:36 +0300

    DONE WITH VECTORS

--- src/vector/Vector_demo.java
+++ src/vector/Vector_demo.java
@@ -1,6 +1,8 @@
 package vector;
 
-/*Mostly same as arralist but this one is synchronized*/
+/*Mostly same as arralist but this one is synchronized so that it has some methods which arraylist doesnt have*/
+
+/*For more information check arraylist to understand better everything is kinda same*/
 
 public class Vector_demo {
 
