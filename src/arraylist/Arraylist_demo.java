c014d4bfed70b6fe1a516e238e3db0f44a1624cd
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-11 19:46:19 +0300

    DONE WITH THE ARRAYLIST

--- src/arraylist/Arraylist_demo.java
+++ src/arraylist/Arraylist_demo.java
@@ -3,6 +3,7 @@ package arraylist;
 /*It is risizable array, this one is also one of class of collection*/
 
 /*must imports*/
+import java.util.*;
 
 /*main class*/
 public class Arraylist_demo {
@@ -11,13 +12,59 @@ public class Arraylist_demo {
 	public static void main(String[] args) {
 
 		/*creating arraylist*/
-		/*adding some objects with index*/
-		/*displaying the arraylist*/
-		/*clearing all elements of arraylist*/
-		/*displaying the arraylist*/
-		/*adding some objects without using index*/
-		/*displaying the arraylist*/
+		ArrayList<String> bag = new ArrayList<>();
 		
+		/*adding some objects with index*/
+		bag.add(0, "Pen");
+		bag.add(1, "Book");
+		bag.add(2, "Pencil");
+		bag.add(3, "Eraser");
+		bag.add(4, "Gum");
+		
+		
+		/*displaying the arraylist*/
+		System.out.println(bag);
+		
+		/*clearing all elements of arraylist*/
+		bag.clear();
+		
+		/*displaying the arraylist*/
+		System.out.println(bag);
+		
+		/*checking if the arraylist is empty or not*/
+		System.out.println(bag.isEmpty());
+		
+		/*adding some objects without using index*/
+		bag.add("Pen");
+		bag.add("Book");
+		bag.add("Pencil");
+		bag.add("Eraser");
+		bag.add("Gum");
+		
+		
+		/*displaying the arraylist*/
+		System.out.println(bag);
+		
+		/*checking if the arraylist contains a particular object*/
+		System.out.println(bag.contains("Dog"));
+		
+		/*getting a particulat object from arraylist using its index*/
+		System.out.println(bag.get(4));
+		
+		/*removing a particular object from the arraylist*/
+		bag.remove("Pen");
+		
+		/*displaying the arraylist*/
+		System.out.println(bag);
+		
+		/*replacing a particular object with another particular object in the arraylist*/
+		bag.set(3, "Chocolate");
+		
+		/*displaying the arraylist*/
+		System.out.println(bag);
+		
+		/*displaying the size of the arraylist*/
+		System.out.println(bag.size());
 		
 	}
 
