fda1d3b1447f9fd54ebdd592298e45cdab98e57f
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-11 20:29:47 +0300

    WORKING WITH THREAD

--- src/thread/Extend_thread.java
+++ src/thread/Extend_thread.java
@@ -1,10 +1,20 @@
 package thread;
 
-public class Extend_thread {
+/*main class must extend thread class*/
+public class Extend_thread extends Thread{
 
+	/*creating a run methods which says something*/
+	public void run() {
+		System.out.println("Thread started running...");
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		/*creating object of the class*/
+		Extend_thread object = new Extend_thread();
+		
+		/*calling the run method using the object of the class*/
+		object.run();
 	}
 
 }
