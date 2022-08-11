dec13e841eb64fc89edfbbae2248e469acdae87c
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-11 19:31:18 +0300

    DEALING WITH ARRAYLIST

---
+++ src/arraylist/Arraylist_demo.java
@@ -0,0 +1,24 @@
+package arraylist;
+
+/*It is risizable array, this one is also one of class of collection*/
+
+/*must imports*/
+
+/*main class*/
+public class Arraylist_demo {
+
+	/*main derived class*/
+	public static void main(String[] args) {
+
+		/*creating arraylist*/
+		/*adding some objects with index*/
+		/*displaying the arraylist*/
+		/*clearing all elements of arraylist*/
+		/*displaying the arraylist*/
+		/*adding some objects without using index*/
+		/*displaying the arraylist*/
+		
+		
+	}
+
+}
---
+++ src/wrapper_class/Wrapper_class_demo.java
@@ -0,0 +1,16 @@
+package wrapper_class;
+
+/*They are used to convert primitive data types to object because sometimes we need primitive data
+ * types as object so we can call them with method*/
+
+/*must imports*/
+import java.util.*;
+
+public class Wrapper_class_demo {
+	
+	/*SYNATAX OF WRAPPER CLASS*/
+	ArrayList<Integer> arraylist = new ArrayList<>(); //here <Ineteger> is a wrapper class
+	
+	/*FOR GOOD UNDERSTANDING CHECK THE COLLECTIONS*/
+	
+}
