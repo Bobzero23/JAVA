75cf7b5b608b99ac8b75754bcb0643fe9cb352f6
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-12 20:27:24 +0300

    DONE WITH QUEUE INTERFACE

--- src/queue/Queue_demo.java
+++ src/queue/Queue_demo.java
@@ -4,6 +4,7 @@ package queue;
  * we need a class to create object of it and linkedlist and priorityqueue are popular*/
 
 /*must import*/
+import java.util.*;
 
 /*main class*/
 public class Queue_demo {
@@ -12,12 +13,32 @@ public class Queue_demo {
 	public static void main(String[] args) {
 
 		/*creating object of the queue*/
+		Queue<Integer> queue = new LinkedList<>();
+		
 		/*adding Integer objects from 1-5 using for loop*/
+		for(int i = 1; i <= 5; i++) {
+			queue.add(i);
+		}
+		
 		/*displaying the queue*/
-		/*creating object for remove() method*/
+		System.out.println(queue);
+		
+		/*creating a variable for remove() method*/
+		/*the variable type is int bcoz our queue is integer*/
+		int rem = queue.remove();
+		
 		/*displaying the removed object*/
+		System.out.println("The removed object is: " + rem);
+		
 		/*creating the object of size() method*/
+		int siz = queue.size();
+		
 		/*displaying the size of the queue*/
+		System.out.println("The size of the queue is: " + siz);
+		
+		/*you can also use the method this way*/
+		System.out.println("The head of the queue is: " + queue.peek());
+		
 	}
 
 }
