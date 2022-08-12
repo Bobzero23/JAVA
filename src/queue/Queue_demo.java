057c9c049b1a50c9ea050c5b05c882ceabe8cd2b
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-12 20:07:55 +0300

    WORKING WITH QUEUE INTERFACE

---
+++ src/queue/Queue_demo.java
@@ -0,0 +1,23 @@
+package queue;
+
+/*This interface supports first in first out concept, since queue is an interface
+ * we need a class to create object of it and linkedlist and priorityqueue are popular*/
+
+/*must import*/
+
+/*main class*/
+public class Queue_demo {
+
+	/*main method*/
+	public static void main(String[] args) {
+
+		/*creating object of the queue*/
+		/*adding Integer objects from 1-5 using for loop*/
+		/*displaying the queue*/
+		/*creating object for remove() method*/
+		/*displaying the removed object*/
+		/*creating the object of size() method*/
+		/*displaying the size of the queue*/
+	}
+
+}
