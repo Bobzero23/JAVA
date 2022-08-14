b3bebb1623a7f627187be6570b6d45efdcf9f9cf
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-14 16:06:00 +0300

    DONE WITH ENUM SET

---
+++ src/set/Enum_set.java
@@ -0,0 +1,26 @@
+package set;
+
+/*imports*/
+import java.util.*;
+
+/*main class*/
+public class Enum_set {
+
+	/*creating Enum in a list form*/
+	enum DAY {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY};
+	
+	/*main method*/
+	public static void main(String[] args) {
+
+		/*declaring object of Set interface with object type of the enum we created*/
+		Set<DAY> day;
+		
+		/*initializing the object with the enums*/
+		day = EnumSet.of(DAY.MONDAY, DAY.TUESDAY, DAY.WEDNESDAY, DAY.THURSDAY, DAY.FRIDAY, DAY.SATURDAY, DAY.SUNDAY);
+		
+		/*displaying the set*/
+		System.out.println("This is our Enum set: " + day);
+		
+	}
+
+}
