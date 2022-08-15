53d7a98f86ddd2e159a9691f57720e0f584427cf
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-15 09:48:09 +0300

    WORKING WITH LIST ITERATOR

---
+++ src/list_iterator/List_iterator.java
@@ -0,0 +1,21 @@
+package list_iterator;
+
+/*This interface extends iterator but the difference is that, 
+ * this one can iterate bi-directional, I mean forward and backward*/
+
+/*imports*/
+
+/*main class*/
+public class List_iterator {
+
+	/*main method*/
+	public static void main(String[] args) {
+
+		/*creating object of arraylist*/
+		/*adding some objects into it*/
+		/*iterating forward using list itertor*/
+		/*iterating backward using list iterator*/
+		
+	}
+
+}
