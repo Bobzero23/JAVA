d3cc9e7fbee8bc3249dd086a44391e08f6a55548
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-16 18:35:37 +0300

    DONE WITH THE COMMAND LINE SIMPLE PROJECT

--- src/maven/Notes.java
+++ src/maven/Notes.java
@@ -13,6 +13,17 @@
  * CREATING A SIMPLE MAVEN PROJECT ON COMMADN LINE
  * Open  a command line and type mvn archtype:generate
  * After typing this you will get a suggestion for a quick start project in a commandline
- * just click enter if you wanna do it 
+ * just click enter if you wanna do it, but if yo gonna play this make sure you have opened the command line 
+ * in a particular file you wanna create this project
+ * 
+ * INSIDE THE COMMAND LINE PROJECT 
+ * this is example on how to do that project
+ * groupid = com.maven_demo --> take this as package
+ * artifactId = Maven demo --> take this as project name
+ * snapshot = 2.4.5 --> take this as a version of your project development 
+ * package name = you can just take as it is or just add some stuffs then you are good to go
+ * after that they will ask you to confirm just type Y and then click enter
+ * after you done all of this steps the project will be created inside the folder you opened command line
+ * 
  * 
  */
