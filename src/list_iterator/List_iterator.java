02dd36833164685ae13f68dbffd7d9004e77df20
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-16 18:21:16 +0300

    WORKING ON A MAVEN COMMAND LINE PROJECT

--- src/list_iterator/List_iterator.java
+++ src/list_iterator/List_iterator.java
@@ -4,6 +4,9 @@ package list_iterator;
  * this one can iterate bi-directional, I mean forward and backward*/
 
 /*imports*/
+import java.util.*;
+
+import javax.swing.JSpinner.ListEditor;
 
 /*main class*/
 public class List_iterator {
@@ -12,9 +15,28 @@ public class List_iterator {
 	public static void main(String[] args) {
 
 		/*creating object of arraylist*/
+		List<String> names = new ArrayList<>();
+		
 		/*adding some objects into it*/
+		names.add("Bobzero");
+		names.add("TheProblem");
+		names.add("Ninja");
+		names.add("GTA");
+		
+		/*creating object of listIterator*/
+		ListIterator<String> name = names.listIterator();
+		
 		/*iterating forward using list itertor*/
+		System.out.println("DISPLAYING FORWARDS");
+		while(name.hasNext()) {
+			System.out.println(name.next());
+		}
+		
 		/*iterating backward using list iterator*/
+		System.out.println("\nDISPLAYING BACKWARDS");
+		while(name.hasPrevious()) {
+			System.out.println(name.previous());
+		}
 		
 	}
 
---
+++ src/maven/Notes.java
@@ -0,0 +1,18 @@
+/* MAVEN 
+ * This platform is provided by apache so that we can work with java based program, like to get jar files etc.
+ *
+ * ENVIRONMENT SET UP
+ * Make sure you have the JKD on your machine
+ * Then download maven on its website
+ * After that you gonna have to set environment paths
+ * 
+ * LITTLE CHECK UP
+ * To see if you have JKD in your machine just simply open command line and type jave -version
+ * To see that you have maven in you machine simply open command line and type (damn I forgot the command)
+ * 
+ * CREATING A SIMPLE MAVEN PROJECT ON COMMADN LINE
+ * Open  a command line and type mvn archtype:generate
+ * After typing this you will get a suggestion for a quick start project in a commandline
+ * just click enter if you wanna do it 
+ * 
+ */
