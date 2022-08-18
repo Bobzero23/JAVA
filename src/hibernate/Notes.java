009f02c12a6279e1450e56a94a449272391fb265
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-18 07:31:51 +0300

    STARTED WORKING WITH ORM HIBERNATE

---
+++ src/hibernate/Notes.java
@@ -0,0 +1,12 @@
+/* ORM
+ * Object Relation Mapping
+ * 
+ * When we are using database sometimes we use tables and stuff
+ * but we cant use it efficenly becuase there must be a mapping too which allow us to make a relation
+ * between object in the java class and the fields in the database relation side so we can access every
+ * field efficiencly and that is why we need an ORM and the best tool for now to use with ORM is HIBERNATE
+ * 
+ *
+ * 
+ * 
+ */
