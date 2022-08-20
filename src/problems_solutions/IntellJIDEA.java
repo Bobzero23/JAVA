2a67d98cda32acb6e96963051787e34622ab7216
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-20 18:47:43 +0300

    WELCOME TO INTELLJ IDEA

---
+++ src/problems_solutions/IntellJIDEA.java
@@ -0,0 +1,7 @@
+/*CANT RUN PROJECTS
+* It took me a whole day to figure this out till I run a cross a hero on YouTube
+* here is the link thanks to this guy I could do some little magic, and also I create a java path myself
+* after looking for how to find the jdk path on stack overflow is easy simply type this in your cmd
+* for %i in (javac.exe) do @echo.   %~$PATH:i
+* then follow this hero on YouTube
+* https://www.youtube.com/watch?v=btZJuhHWc10*/
