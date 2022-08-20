4ae4c54ca2ebb7f55e2abd4b9d9d28aa21fdfbad
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-20 08:50:00 +0300

    STARTED SPRING

---
+++ src/spring/Notes.java
@@ -0,0 +1,17 @@
+/* SPRING
+ * Is a framework which is so useful and popular for java developer in terms of creating web applications
+ * 
+ * WHY WE SHOULD USE A FRAMEWORK
+ * Because they make things easier we only have to focus on building our stuffs and the other stuffs come
+ * ready from the frame works..
+ * 
+ * PROJECTS OF SPRING FRAME WORK
+ * Spring Core & MVC this is like core Spring
+ * Spring Boot: for creating complex web applications
+ * Sprin Data: for dealing with databases
+ * Spring Cloud: for working with platforms like Docker etc.
+ * Spring Security: for security
+ * 
+ * 
+ */
+
