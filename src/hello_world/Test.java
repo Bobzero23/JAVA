34dad8eb5b145b84a3bc3fedbbe42bcc5ed8ffdc
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-07 12:44:09 +0300

    PRACTICING

--- src/hello_world/Test.java
+++ src/hello_world/Test.java
@@ -1,17 +1,18 @@
 package hello_world;
 
+/*Given a string, if the string "del" appears starting at index 1,
+ return a string where that "del" has been deleted. Otherwise,
+ return the string unchanged.*/
 
 public class Test {
 
-    static String missingChar(String str, int n){
-
-        String front = str.substring(0 , n);
-        String back = str.substring(n + 1, str.length());
-        return front + back;
+  static String delDel(String str) {
+        if(str.length() >= 4 && str.substring(1,4).equals("del")){
+            return str.charAt(0) + str.substring(4);
+        }else return str;
     }
 
     public static void main(String[] args) {
-
-        System.out.println(missingChar("Bobzero", 5));
+        System.out.println(delDel("Bdelobzero"));
     }
 }
