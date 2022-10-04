69744ae80bb522abcce3cf09cc6326d4c2b90e20
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-04 19:06:47 +0300

    PADDING INTEGER WITH ZEROS

---
+++ src/padding/PaddingInt.java
@@ -0,0 +1,29 @@
+package padding;
+
+/*Demonstration on how to pad integer with zero*/
+/*Let's say we want to print integer with exactly 5 digits but the user inter the integer with 3 digits so we gonna
+* hava to pad two zeros on the left to make it 5 digits right, check what I did below*/
+
+import java.util.Scanner;
+
+/*main class*/
+public class PaddingInt {
+
+    /*padding method*/
+    public String paddingInt(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a positive number with 5 digits: ");
+        int input = scan.nextInt();
+        return String.format("%05d", input);
+    }
+
+    /*main method*/
+    public static void main(String[] args) {
+
+        PaddingInt object = new PaddingInt();
+        String result = object.paddingInt();
+        System.out.println(result);
+
+    }
+
+}
