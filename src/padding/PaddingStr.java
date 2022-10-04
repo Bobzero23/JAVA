66d06c22ea756edac3e72bfaca835426a7ea5183
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-04 19:14:16 +0300

    PADDING STRINGS WITH SPACES

---
+++ src/padding/PaddingStr.java
@@ -0,0 +1,15 @@
+package padding;
+
+/*Demonstration on how to pad space in Strings*/
+
+/*main class*/
+public class PaddingStr {
+
+    /*main method*/
+    public static void main(String[] args) {
+
+        String name = "Bobzero";
+        String surname = "TheProblem";
+        System.out.printf("%-10s %s", name, surname);
+    }
+}
