0328f3238e43789452539ac70f8797c057a8d4fd
Bobzero <rajabu.mustafa.00@gmail.com>
2022-12-13 12:36:48 +0300

    HOW TO USE AND THEN

--- src/FUNCTIONAL/_Function.java
+++ src/FUNCTIONAL/_Function.java
@@ -1,4 +1,41 @@
 package FUNCTIONAL;
 
+/*EXAMPLE ON HOW TO USE JAVA UTIL FUNCTIONS*/
+
+/*imports*/
+import java.util.function.Function;
+
 public class _Function {
+
+    /*THIS IS HOW WE USED TO DO*/
+    static int increment(int number) {
+        return number + 1;
+    }
+
+    /*THIS IS HOW NOW IT IS DONE*/
+    /*<T, R> stands for data types of the input and the output*/
+    /*number is the variable name of the input*/
+    static Function<Integer, Integer> IncrementByOneFunction = number -> number + 1;
+
+    /*function for multiplication*/
+    static Function<Integer, Integer> multiplyByTen = number -> number * 10;
+
+    public static void main(String[] args) {
+       int result1 = increment(9);
+        System.out.println(result1);
+
+        /*HOW TO USE APPLY*/
+        int applyIncrement = IncrementByOneFunction.apply(5);
+        System.out.println(applyIncrement);
+
+        int multiply = multiplyByTen.apply(applyIncrement);
+        System.out.println(multiply);
+
+        /*How to use AND THEN*/
+        /*we normally use this to put two or more method work together*/
+        Function<Integer, Integer> incrementAndThenMultiply =
+                IncrementByOneFunction.andThen(multiplyByTen);
+
+        System.out.println(incrementAndThenMultiply.apply(4));
+    }
 }
