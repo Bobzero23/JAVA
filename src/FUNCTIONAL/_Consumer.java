fe00ce8d8dd84804f633174a81320986f6850698
Bobzero <rajabu.mustafa.00@gmail.com>
2022-12-14 20:53:22 +0300

    CONSUMER AND BI CONSUMER FUNCTIONAL INTERFACE

--- src/FUNCTIONAL/_Consumer.java
+++ src/FUNCTIONAL/_Consumer.java
@@ -1,5 +1,6 @@
 package FUNCTIONAL;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
@@ -23,7 +24,7 @@ public class _Consumer {
         /*creating a new costumer*/
         Costumer costumer1 = new Costumer("maria", 526526);
 
-        /*calling a consumer interface*/
+        /*calling a consumer interface inside the normal function*/
         greetCostumerConsumer.accept(costumer1);
     }
 
@@ -32,7 +33,23 @@ public class _Consumer {
             System.out.println("Hello " + costumer.costumerName + ", Thank you for registering phone number "
             + costumer.costumerPhoneNumber);
 
+    /*USING BI CONSUMER INTERFACE*/
+    static BiConsumer<Costumer, Boolean> greetCostumerBiConsumer = (costumer, showPhoneNumber) ->
+            System.out.println("Hello " + costumer.costumerName +
+                    " Thanks for registering phone number " +
+                    (showPhoneNumber ? costumer.costumerPhoneNumber : "*****"));
+
     public static void main(String[] args) {
+        /*creating the costumer*/
+        Costumer Bobzero = new Costumer("Bobzero", 748748);
+
+        /*calling the normal method*/
         greetCostumer(new Costumer("Bobzero",415415));
+
+        /*calling the consumer method*/
+        greetCostumerConsumer.accept(Bobzero);
+
+        /*calling the bi consumer method*/
+        greetCostumerBiConsumer.accept(Bobzero, true);
     }
 }
