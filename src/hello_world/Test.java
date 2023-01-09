db1bd68a2046136b7e03fea2c1eab1a8229cd6ee
Bobzero <rajabu.mustafa.00@gmail.com>
2023-01-09 14:30:51 +0300

    CODE REVIEW

--- src/hello_world/Test.java
+++ src/hello_world/Test.java
@@ -1,8 +1,42 @@
 package hello_world;
 
+import java.util.List;
+
+import static hello_world.Test.Gender.FEMALE;
+import static hello_world.Test.Gender.MALE;
+
 public class Test {
 
-    public static void main(String[] args) {
+    enum Gender {
+        MALE, FEMALE
+    }
 
+    public static class Human{
+        private final String name;
+        private final Gender gender;
+
+        public Human(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Human{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+    public static void main(String[] args) {
+        List<Human> human_beings =  List.of(
+            new Human("Bobzero", MALE),
+            new Human("Kombo", FEMALE),
+            new Human("Maho", FEMALE)
+        );
+
+        human_beings.stream()
+                .filter(human -> FEMALE.equals(human.gender))
+                .forEach(System.out::println);
     }
 }
