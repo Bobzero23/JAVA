b3f6cf634f0fa6b9845e32662797011cf4c5f2e0
Bobzero <rajabu.mustafa.00@gmail.com>
2023-01-10 09:50:55 +0300

    CODE REVIEW

--- src/hello_world/Test.java
+++ src/hello_world/Test.java
@@ -5,15 +5,18 @@ import java.util.function.Function;
 
 public class Test {
 
-    static Function<Integer, Integer> sampleFunction = number -> number * 10;
-    static Function<Integer, Integer> sampleFunction1 = number -> number / 10;
-
+    static Function<Integer, Integer>   incrementFunction = number -> number + 10;
+    static Function<Integer, Integer> multiplyFunction1 = number -> number * 10;
+    static Function<Integer, Integer> IncrementMultiplyFunction =
+            incrementFunction.andThen(multiplyFunction1);
 
     public static void main(String[] args) {
-        int result = sampleFunction.apply(10);
-        int result1 = sampleFunction1.apply(100);
+        int result = incrementFunction.apply(5);
+        int result1 = multiplyFunction1.apply(1);
+        int result2 = IncrementMultiplyFunction.apply(1);
 
-        System.out.println(result1);
         System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
