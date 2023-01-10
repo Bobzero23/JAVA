060ec4ccc38f59994a518b538480e61f3598e786
Bobzero <rajabu.mustafa.00@gmail.com>
2023-01-10 09:37:04 +0300

    CODE REVIEW

--- src/hello_world/Test.java
+++ src/hello_world/Test.java
@@ -1,42 +1,19 @@
 package hello_world;
 
-import java.util.List;
 
-import static hello_world.Test.Gender.FEMALE;
-import static hello_world.Test.Gender.MALE;
+import java.util.function.Function;
 
 public class Test {
 
-    enum Gender {
-        MALE, FEMALE
-    }
+    static Function<Integer, Integer> sampleFunction = number -> number * 10;
+    static Function<Integer, Integer> sampleFunction1 = number -> number / 10;
 
-    public static class Human{
-        private final String name;
-        private final Gender gender;
 
-        public Human(String name, Gender gender) {
-            this.name = name;
-            this.gender = gender;
-        }
-
-        @Override
-        public String toString() {
-            return "Human{" +
-                    "name='" + name + '\'' +
-                    ", gender=" + gender +
-                    '}';
-        }
-    }
     public static void main(String[] args) {
-        List<Human> human_beings =  List.of(
-            new Human("Bobzero", MALE),
-            new Human("Kombo", FEMALE),
-            new Human("Maho", FEMALE)
-        );
+        int result = sampleFunction.apply(10);
+        int result1 = sampleFunction1.apply(100);
 
-        human_beings.stream()
-                .filter(human -> FEMALE.equals(human.gender))
-                .forEach(System.out::println);
+        System.out.println(result1);
+        System.out.println(result);
     }
 }
