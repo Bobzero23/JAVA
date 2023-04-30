58fcea760f52d34e15e006efd5eaf18a7bf1c2e2
Bobzero <rajabu.mustafa.00@gmail.com>
2023-04-30 19:11:47 +0300

    implementing streams

---
+++ src/streams/example1.java
@@ -0,0 +1,14 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class example1 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Murat", "Umit", "Recep", "Burak", "Rajabu", "Ayse", "Rashid", "Rahma");
+        List<String> myStreamList = list.stream().sorted(Comparator.reverseOrder())
+                .filter(item -> item.startsWith("R")).map(String::toUpperCase).toList();
+        myStreamList.forEach(System.out::println);
+    }
+}
